76b6066a8c423e473bf948e8b3bcda9f6866164b
Sunny <sanidhyakhanna123@gmail.com>
2025-10-25 20:59:49 -0400

    train.py finished ready for training using modal

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ import sys
 from model import AudioCNN
 import torch.optim  as optim
 from torch.optim.lr_scheduler import OneCycleLR
+from tqdm import tqdm
+import numpy as np
 
 app = modal.App("audio-CNN")
 
@@ -68,6 +70,19 @@ class ESC50Dataset(Dataset):
         return spectogram, row['label']
 
 
+def mixup_data(x,y):
+    lam = np.random.beta(0.2,0.2)
+    batch_size = x.size(0)
+    index = torch.randperm(batch_size).to(x.device)
+
+    #to create synthetic sample 30 percent of first audio clip plus 70 percent of second audio clip
+    mixed_x = lam * x + (1 - lam) * x[index,:]
+    y_a,y_b = y,y[index]
+    return mixed_x,y_a,y_b,lam
+
+def mixup_criterion(criterion,pred,y_a,y_b,lam): #calc loss
+    return lam * criterion(pred,y_a) + (1 - lam) * criterion(pred,y_b)
+
 
 
 @app.function(image=image,gpu="A10G",volumes={"/data":data_volume,"/models":model_volume},timeout= 60 * 60 * 3)
@@ -96,7 +111,7 @@ def train():
 
     # load data in batches to train nn
     train_dataloader = DataLoader(train_dataset,batch_size=32,shuffle=True)
-    val_loader = DataLoader(val_dataset,batch_size=32,shuffle=False)
+    test_dataloader = DataLoader(val_dataset,batch_size=32,shuffle=False)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = AudioCNN(num_classes=len(train_dataset.classes))
@@ -106,7 +121,75 @@ def train():
     criterion = nn.CrossEntropyLoss(label_smoothing=0.1) # label smoothing helps model to be more humble in its predictions 
     optimizer = optim.AdamW(model.parameters(),lr=0.005,weight_decay=0.001)
     
-    scheduler = One
+    scheduler = OneCycleLR(
+        optimizer,
+        max_lr = 0.002,
+        epochs=num_epochs,
+        steps_per_epoch=len(train_dataloader),
+        pct_start=0.1
+    )
+
+    best_accuracy = 0.0
+    print("Starting training")
+    for epoch in range(num_epochs):
+        model.train()
+        epoch_loss = 0.0
+
+        progress_bar = tqdm(train_dataloader,desc=f"Epoch {epoch +1}/{num_epochs}")
+        for data,target in progress_bar:
+            data,target = data.to(device),target.to(device)
+
+            if np.random.random() > 0.7:
+                data,target_a, target_b, lam = mixup_data(data,target)
+                output = model(data)
+                loss = mixup_criterion(criterion,output,target_a,target_b,lam)
+            else:
+                output = model(data)
+                loss = criterion(output,target)
+            
+            optimizer.zero_grad()
+            loss.backward() #self adjust the weights for model improvement 
+            optimizer.step()
+            scheduler.step()
+
+            epoch_loss += loss.item()
+            progress_bar.set_postfix({'Loss': f'{loss.item():.4f}'})
+
+        avg_epoch_loss = epoch_loss / len(train_dataloader)
+
+        #validation after each epoch
+        model.eval()
+
+        correct = 0
+        total = 0
+        val_loss = 0
+
+        with torch.no_grad():
+            for data,target in test_dataloader:
+                data, target = data.to(device), target.to(device)
+                outputs = model(data)
+                loss = criterion(data)
+                val_loss += loss.item()
+
+                _, predicted = torch.max(outputs.data,1)
+                total += target.size(0)
+                correct += (predicted == target).sum().item()
+
+        accuracy = 100 * correct / total
+        avg_val_loss = val_loss / len(test_dataloader)
+        
+        print(f'Epoch {epoch + 1} Loss: {avg_epoch_loss:.4f}, Val loss: {avg_val_loss:.4f}, Acurracy: {accuracy:.2f}')
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'accuracy': accuracy,
+                'epoch': epoch,
+                'classes': train_dataset.classes
+            }, '/models/best_model.pth')
+            print(f"New best model saved: {accuracy:.2f}")
+    print(f'Training completed and the Best accuracy:{best_accuracy:.2f}%')
 
 
 @app.local_entrypoint()
